1e30b7b50846eecc0556cea2e96b6524bf284b12
eppzx2 <elmirinfo351@gmail.com>
2025-11-19 11:38:17 +0000

    error fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,13 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for i in range(x): #listin indexi qeder dovr
+        for i in range(x):
             try:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-            except (ValueError, TypeError): #element int deyilse kec
+            except (ValueError, TypeError):
                 pass
-    except IndexError: #mylistin indeksinden coxdursa problem eleme
+    except IndexError:
         pass
     print()
     return count
