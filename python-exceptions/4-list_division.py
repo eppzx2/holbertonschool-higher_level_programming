304938d735f0fd5d88b0129d566f9941705d5918
eppzx2 <elmirinfo351@gmail.com>
2025-11-19 12:23:39 +0000

    list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -16,6 +16,9 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
         finally:
             pass
     return result
