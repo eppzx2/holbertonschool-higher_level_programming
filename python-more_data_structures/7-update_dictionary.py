84f219e0c1dda9b708ff57952474bb80d3cd53eb
eppzx2 <elmirinfo351@gmail.com>
2025-11-19 08:47:04 +0000

    updating dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        a_dictionary = {}
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
