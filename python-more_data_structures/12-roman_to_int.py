a979eb720e905ec93cb58e7fd725dc7c208e50e8
eppzx2 <elmirinfo351@gmail.com>
2025-11-19 09:32:36 +0000

    roman to integer task

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dic = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+    total = 0
+    lenght = len(roman_string)
+    for i in range(lenght):
+        value = roman_dic[roman_string[i]]
+        if i + 1 < lenght and value < roman_dic[roman_string[i + 1]]:
+            total -= value
+        else:
+            total += value
+    return total
