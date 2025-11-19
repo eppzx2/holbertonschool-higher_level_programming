1ba29213274138dc82a78a9f00f48ba6a358d857
eppzx2 <elmirinfo351@gmail.com>
2025-11-19 08:55:22 +0000

    deleting key

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is None:
-        a_dictionary = {}
-    for key in sorted(a_dictionary.keys()):
-        print(f"{key}: {a_dictionary[key]}")
+    a_dictionary[key] = value
