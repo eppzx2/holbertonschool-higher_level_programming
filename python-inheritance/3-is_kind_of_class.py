5622cd2ff2746a8f561e7e3f27808c30fb2f2eb3
eppzx2 <elmirinfo351@gmail.com>
2025-11-25 09:09:28 +0000

    is kind of class

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,4 +6,4 @@ this func returns true if object is an instance of a spec class
 
 def is_kind_of_class(obj, a_class):
     """if isinstance obj in a class return true"""
-    return isinstance(obj, a_class
+    return isinstance(obj, a_class)
