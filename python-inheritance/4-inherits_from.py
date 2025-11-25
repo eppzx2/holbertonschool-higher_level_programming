b1d426ecae75ba9dd1df9630fca0917a0f592d55
eppzx2 <elmirinfo351@gmail.com>
2025-11-25 09:13:44 +0000

    inherited from the specified class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+inheritance training
+"""
+
+
+def inherits_from(obj, a_class):
+    """inherited from the specified class"""
+    return type(obj) != a_class and isinstance(obj, a_class)
