8d8df38ee5a6d215980c99f6fe26138bc6d057ff
eppzx2 <elmirinfo351@gmail.com>
2025-11-25 09:15:45 +0000

    inherited from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ inheritance training
 
 def inherits_from(obj, a_class):
     """inherited from the specified class"""
-    return type(obj) != a_class and isinstance(obj, a_class)
+    return type(obj) is not a_class and isinstance(obj, a_class)
