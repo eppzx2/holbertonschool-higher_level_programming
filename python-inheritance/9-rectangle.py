d3c218c9b506e46070d047a16dff6e3e740de625
eppzx2 <elmirinfo351@gmail.com>
2025-11-25 09:45:00 +0000

    integer validator

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module defining Rectangle class inheriting from BaseGeometry
+"""
+
+
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
