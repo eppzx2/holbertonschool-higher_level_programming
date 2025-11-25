1e61d38a8860ccd71dda7a372083445d1c8abfe7
eppzx2 <elmirinfo351@gmail.com>
2025-11-25 07:23:16 +0000

    rectangle class

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-defining a rectangle but not empty
+error handling with classes
 """
 
 
 class Rectangle:
-    """property and property setter"""
+    """class with perimeter and area function"""
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-
+    
     @property
     def width(self):
         return self.__width
@@ -21,7 +21,6 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def height(self):
@@ -33,4 +32,12 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
