b6efbafadbc9cc5e81c4e4326f05e1a6af5d943d
eppzx2 <elmirinfo351@gmail.com>
2025-11-26 11:22:11 +0000

    csv json dataset

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -20,5 +20,5 @@ def convert_csv_to_json(csv_filename):
         with open("data.json", "w", encoding="utf-8") as json_file:
             json.dump(data_list, json_file, indent=4)
         return True
-     except Exception:
+   except Exception:
         return False
