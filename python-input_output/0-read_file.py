d40a20c7a480af8a00bad0f27e2882ccb0991317
eppzx2 <elmirinfo351@gmail.com>
2025-11-26 06:00:11 +0000

    opening and reading a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,6 @@ fayli acir oxuyur bagliyir
 
 def read_file(filename=""):
     """with ozu acir is bitende bagliyir f.close ehtiyac yoxdu"""
+
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
