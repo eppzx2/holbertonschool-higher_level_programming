e9b690cd2bc75dd570da8197b287a3f9ec775eaa
eppzx2 <elmirinfo351@gmail.com>
2025-11-26 09:43:51 +0000

    adding to a list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ adds all arguments to a python list
 
 import sys
 from pathlib import Path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
