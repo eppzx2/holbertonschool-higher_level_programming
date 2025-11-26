92d23ac0ea2e63c56de89f052f77ff3e3d4e50d5
eppzx2 <elmirinfo351@gmail.com>
2025-11-26 06:25:43 +0000

    from json to python object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+jsonla bagli emeliyyatlarda import
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """json strdan pythona cevirerken json.loads"""
+
+    return json.loads(my_str)
