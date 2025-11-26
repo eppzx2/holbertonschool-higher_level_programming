de97a299cf951c913fd348ec8545b1faf9306f67
eppzx2 <elmirinfo351@gmail.com>
2025-11-26 10:17:32 +0000

    more student class

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+defining a class
+"""
+
+
+class Student:
+    """
+    A class representing a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__.copy()
+
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
