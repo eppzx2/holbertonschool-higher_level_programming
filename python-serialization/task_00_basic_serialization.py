42374ddee4a55090b2d7ea8f16463bd5f042208b
eppzx2 <elmirinfo351@gmail.com>
2025-11-26 11:07:52 +0000

    serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""first serialization"""
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
