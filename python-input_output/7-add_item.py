46f4351775b053de238e9fd7b75561af30b4321f
eppzx2 <elmirinfo351@gmail.com>
2025-11-26 09:57:12 +0000

    import multiple

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 """
-adds all arguments to a python list
-"""
+importimport
+""" 
 
 
 import sys
+import importlib.util
 from pathlib import Path
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+
+spec = importlib.util.spec_from_file_location(
+    "save_to_json_file", "./5-save_to_json_file.py"
+)
+save_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(save_module)
+
+
+spec2 = importlib.util.spec_from_file_location(
+    "load_from_json_file", "./6-load_from_json_file.py"
+)
+load_module = importlib.util.module_from_spec(spec2)
+spec2.loader.exec_module(load_module)
 
 filename = "add_item.json"
+items = []
 
-
-if Path(filename).exists():
-    items = load_from_json_file(filename)
-else:
-    items = []
-
+if Path(filename).is_file():
+    items = load_module.load_from_json_file(filename)
 
 items.extend(sys.argv[1:])
-
-
-save_to_json_file(items, filename)
+save_module.save_to_json_file(items, filename)
