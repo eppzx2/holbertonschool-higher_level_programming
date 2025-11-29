5fe237222a6f2c9935a1f37212c7cbcf3cd8a789
eppzx2 <elmirinfo351@gmail.com>
2025-11-29 10:22:10 +0000

    difficult task

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,5 @@
 -- a little difficult task
 SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
 FROM tv_show_genres
-JOIN genres g ON tsg.genre_id = g.id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
-
