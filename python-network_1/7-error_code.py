640b84bc95fc3c862f81e403268085684fcad772
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 09:31:54 +0000

    If the HTTP status code is greater than or equal to 400, print: Error code

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Exception handling in requests package
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        response = requests.get(url)
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+        else:
+            print(response.text)
+    except requests.RequestException as e:
+        print(f"Error: {e}")
