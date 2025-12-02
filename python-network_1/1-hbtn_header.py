83c193fe55bd600690a129a383bec72be917c455
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 08:46:45 +0000

    Python script that takes in a URL, sends a request to the URL and displays the value

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!//usr/bin/python3
+""" """
+
+
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    x_req_id = response.getheader('X-Request-Id')
+print(x_req_id)
