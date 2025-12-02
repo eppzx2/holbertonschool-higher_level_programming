9db70888353ad5051135487843659af99ece7000
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 09:10:23 +0000

    script that takes in a URL and an email, sends a POST request to the passed URL with the email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,9 +8,9 @@ import sys
 import urllib.parse
 import urllib.request
 
-
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
 data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 with urllib.request.urlopen(urllib.request.Request(url, data=data)) as response:
