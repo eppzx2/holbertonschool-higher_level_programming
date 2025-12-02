f1402e9249ffe11e34b84c44228b87e1ba0425e8
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 10:09:12 +0000

    github credentials show id

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
+"""
+github credentials login
+"""
 
 
+import sys
 import requests
 
 
-username = "eppzx2"
-token = "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"
+def get_github_user_id(username, token):
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(data['id'])
+    else:
+        print("Error:", response.status_code)
 
-url = "https://api.github.com/user"
-
-response = requests.get(url, auth=(username, token))
-
-if response.status_code == 200:
-    user_data = response.json()
-    print(user_data['id'])
-else:
-    print(f"Failed to retrieve user information. HTTP Status Code: {response.status_code}")
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    
+    get_github_user_id(username, token)
