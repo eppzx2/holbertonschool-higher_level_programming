179e64cd80a4562433025e6e69a60c2154915882
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 09:51:36 +0000

    takes your github credentials and uses github api

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+It sends a GET request to GitHub's /user API endpoint,
+using Basic Authentication (username and token)
+"""
+
+
+import requests
+import sys
+
+
+username = sys.argv[1]
+token = sys.argv[2]
+
+url = "https://api.github.com/user"
+
+response = requests.get(url, auth=(username, token))
+
+if response.status_code == 200:
+    user_data = response.json()
+    print(user_data['id'])
+else:
+    print(f"Failed to retrieve user information. HTTP Status Code: {response.status_code}")
