d840214563fb13464db61966180f40d3d27587f6
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 11:14:27 +0000

    Setting Up a Basic HTTP Server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Setting Up a Basic HTTP Server:
+setting up a http server(hinthunter ;))
 """
 
 
@@ -8,42 +8,68 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    """custom HTTP request handler"""
+
     def do_GET(self):
-        """Handle GET requests"""
+        """handle GET requests"""
+
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-        
+            return
+
         elif self.path == "/data":
-            data = {"name": "John", "age": 30, "city": "New York"}
-            response = json.dumps(data).encode('utf-8')
-            
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            response = json.dumps(data).encode("utf-8")
+
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(response)
-        
+            return
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
-        
+            return
+
+        elif self.path == "/info":
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            response = json.dumps(info).encode("utf-8")
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(response)
+            return
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            message = "Endpoint not found"
+            self.wfile.write(message.encode("utf-8"))
+            return
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f"Starting server on port {port}...")
+
+def run_server():
+    server_address = ("", 8000)
+    httpd = HTTPServer(server_address, SimpleAPIHandler)
+    print("Server running on port 8000...")
     httpd.serve_forever()
 
+
 if __name__ == "__main__":
-    run()
+    run_server()
