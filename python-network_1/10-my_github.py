6fefcac684579083274adc1c52d9b04f04458eac
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 10:58:09 +0000

    displays an id

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-github credentials login
+uses the Github API to display your user id.
 """
 
 
@@ -8,18 +8,16 @@ import sys
 import requests
 
 
-def get_github_user_id(username, token):
-    url = "https://api.github.com/user"
-    response = requests.get(url, auth=(username, token))
-    
-    if response.status_code == 200:
-        data = response.json()
-        print(data['id'])
-    else:
-        print("Error:", response.status_code)
-
 if __name__ == "__main__":
     username = sys.argv[1]
-    token = sys.argv[2]
-    
-    get_github_user_id(username, token)
+    password = sys.argv[2]
+url = "https://api.github.com/user"
+try:
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('id'))
+    else:
+        print("None")
+except requests.RequestException:
+    print("None")
