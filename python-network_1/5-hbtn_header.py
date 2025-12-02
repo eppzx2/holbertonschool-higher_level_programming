71590ece61c9d46fc8444b9724134024e3891ae5
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 09:26:11 +0000

    same tasks with request and sys packages

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+same task with requests package
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    x_request_id = response.headers.get("X-Request-Id")
+
+    print(x_request_id)
