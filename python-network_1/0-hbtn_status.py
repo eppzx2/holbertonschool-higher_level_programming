524b1bd16cb4ee2cbb3f1bbba5281c5b5452bec2
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 08:41:55 +0000

    [pycodestyle] Command to run:

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 fetching an url
-""" 
+"""
 
 
 import urllib.request
 
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    body =  response.read()
+    body = response.read()
 
 print(f"Body response:\n\t- type: {type(body)}")
 print(f"\t- content: {body}")
