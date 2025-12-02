0b784413783fedd169fb288d32df5840c06b2d25
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 09:34:36 +0000

    JSON formatted

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Exception handling and json format together
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    try:
+        response = requests.post(url, data={"q": q})
+        try:
+            result = response.json()
+        except ValueError:
+            print("Not a valid JSON")
+        else:
+            if result:
+                print(f"[{result.get('id')}] {result.get('name')}")
+            else:
+                print("No result")
+    except requests.RequestException:
+        print("No result")
