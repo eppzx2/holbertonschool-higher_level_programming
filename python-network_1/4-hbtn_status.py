1bcbc05734b1ebd4d07d3efcb56463969a1ca151
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 09:24:28 +0000

    fetching url with requests package

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+only requests module
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
