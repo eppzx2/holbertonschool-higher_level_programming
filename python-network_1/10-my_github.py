8b0bf557ea581412b05c6606e71bd42302d78974
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 10:59:24 +0000

    sdkfjs

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -11,13 +11,13 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-url = "https://api.github.com/user"
-try:
-    response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        user_data = response.json()
-        print(user_data.get('id'))
-    else:
+    url = "https://api.github.com/user"
+    try:
+        response = requests.get(url, auth=(username, password))
+        if response.status_code == 200:
+            user_data = response.json()
+            print(user_data.get('id'))
+        else:
+            print("None")
+    except requests.RequestException:
         print("None")
-except requests.RequestException:
-    print("None")
