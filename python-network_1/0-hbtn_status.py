7fd86bddad08a1a87a3df02a1ef0369df29db702
eppzx2 <elmirinfo351@gmail.com>
2025-12-02 08:34:00 +0000

    Python script that fetches https://intranet.hbtn.io/status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,5 +3,5 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     body =  response.read()
 
-print(f"Body response:\n\t- {body}")
+print(f"Body response:\n\t- {body.decode('utf-8')}")
 
